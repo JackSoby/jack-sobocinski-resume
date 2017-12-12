9527e18783618fd627cf4e3063235c2e4f3a056a
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-12-12 14:03:57 -0500

    started to build out projects page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :voas, only: [:index]
   resources :launches, only: [:index]
   resources :market_baskets, only: [:index]
+  resources :projects, only: [:index]
+
 end
